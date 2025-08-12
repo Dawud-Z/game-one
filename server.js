8283fb0f0127d4b47b76db140340e1786342e0d1
Dawud Zarar <130295869+Dawud-Z@users.noreply.github.com>
2025-08-12 08:39:15 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+// [rest of the server code I provided earlier]
